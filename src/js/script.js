807c7e30939c0446feeb4c569b129be0da7e8d48
YuraAlhovka <yury.alhovka@yandex.ru>
2025-07-20 20:27:50 +0300

    feat: section promo

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 import "../sass/style.scss";
+
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  // configure Swiper to use modules
+  modules: [Pagination],
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
